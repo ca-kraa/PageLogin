ba90db13b91d67d90cf6d305a23af2b849f02c64
Nabil <alexandrokingnabil@gmail.com>
2023-06-16 00:46:12 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        ungu : '#8922f3'
+      }
+    },
+  },
+  plugins: [],
+}
+
